f882b398f4cdb53114ade3009c98c07b60a4aa5e
Chris Friedline <cfriedline@vcu.edu>
2014-08-20 00:29:21 -0400

    init

---
+++ Vagrantfile
@@ -0,0 +1,12 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+# Vagrantfile API/syntax version. Don't touch unless you know what you're doing!
+VAGRANTFILE_API_VERSION = "2"
+
+Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
+  config.vm.box = "ubuntu/trusty64"
+  config.vm.hostname = "swc"
+  config.vm.provision "shell", path: "setup.sh", privileged: false
+  config.vm.network "forwarded_port", guest: 8888, host: 9999
+end
---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Update the OS and install Software Carpentry requirements
+sudo apt-get update -y
+sudo apt-get dist-upgrade -y 
+sudo apt-get install -y git sqlite3 zsh r-base 
+
+# Install/update Anaconda and adjust environment
+ANACONDA_HOME=$HOME/anaconda
+wget http://repo.continuum.io/archive/Anaconda-2.0.1-Linux-x86_64.sh
+bash Anaconda-2.0.1-Linux-x86_64.sh -b -p $ANACONDA_HOME
+echo "PATH=$ANACONDA_HOME/bin:$PATH" >> ~/.bashrc
+echo "export PATH" >> ~/.bashrc
+$ANACONDA_HOME/bin/conda update --yes conda
+$ANACONDA_HOME/bin/conda update --yes anaconda
+
+# Make IPython notebook start at boot
+sudo sh -c "echo 'cd /vagrant && $ANACONDA_HOME/bin/ipython notebook --ip=* --no-browser &' > /etc/rc.local"
+sudo sh -c "echo 'exit 0' >> /etc/rc.local"
